7281bcf808e64f3b5d14ceb7d50f21e5b8cd33e7
dnscxrbl <rybalka56@gmail.com>
2024-05-10 21:52:25 +0300

    updWork

--- script1.sh
+++ script1.sh
@@ -1,73 +1,55 @@
-#!/bin/bash
+#!/bin/bash -eu
 
-
-# Отримуємо поточну дату
+# Получаем текущую дату
 current_date=$(date +'%Y/%m/%d')
 
-# Перевірка існування папки /var/SOMA та створення її, якщо вона не існує
+# Проверка существования папки /var/SOMA и создание её, если она не существует
 if [ ! -d "/var/SOMA/$current_date" ]; then
-    mkdir -p /var/SOMA/$current_date
+    mkdir -p "/var/SOMA/$current_date"
 fi
 
-# Перевірка, чи існує папка SOMA в /data
+# Проверка существования папки SOMA в /data
 if [ ! -d "/data/SOMA/$current_date" ]; then
-    mkdir -p /data/SOMA/$current_date
+    mkdir -p "/data/SOMA/$current_date"
 fi
 
-#Створюю файл логів та помилок в /var/SOMA/...
+# Создаю файлы логов и ошибок в /var/SOMA/...
 script_log="/var/SOMA/$current_date/script.log"
 error_log="/var/SOMA/$current_date/error.log"
 result_file="/data/SOMA/$current_date/result_file.txt"
 
-# Записуємо час запуску скрипта в файл script.log
+# Записываем время запуска скрипта в файл script.log
 echo "Script started at $(date)" >> "$script_log"
 
-#Перебираю шляхи та відокремлюю їх одна від одної 
+# Разделение путей и запись их в массив directories
+IFS=':' read -ra directories <<< "$PATH"
+for dir in "${directories[@]}"; do
+    echo "Directory: $dir"
+done
 
-dir=$(IFS=':' read -ra paths <<< "$PATH"
-for path in "${paths[@]}"; do
-    echo "Directory: $path"
-done)
-
-# Цикл для перевірки існування директорій та їх вмісту
-for dir in $directories; do 
-    if [[ -d "$dir" ]]; then 
-        # Підрахунок кількості файлів в директорії 
+# Цикл для проверки существования директорий и их содержимого
+for dir in "${directories[@]}"; do
+    if [[ -d "$dir" ]]; then
+        # Подсчет количества файлов в директории
         files_count=$(find "$dir" -type f | wc -l)
         echo "Directory: $dir, File count: $files_count" >> "$result_file"
     else
         echo "Directory $dir does not exist" >> "$error_log"
-    fi 
-done 
+    fi
+done
 
-echo "Script finished at $(date)" >> "$result_file"
+# Поиск файлов с результатами в /data/SOMA и запись их в переменную result_files
+result_files=$(find "/data/SOMA/$current_date" -name "result*.txt")
 
-# Знаходимо шляхи до файлів з результатами виконання скрипта
-#result_files=$(find . -name "result*.txt")
-
-# Перебираємо кожен файл з результатами
+# Перебор каждого файла с результатами
 for file in $result_files; do
-    # Знаходимо директорію з PATH у файлі
-    path_directory=$(grep -R "/usr/sbin" "$file" | awk -F ':' '{print $1}')
-    # Якщо директорія знайдена, виводимо інформацію
+    # Поиск директории с PATH в файле
+    path_directory=$(grep -R "$dir" "$file" | awk -F ':' '{print $1}')
+    # Если директория найдена, выводим информацию
     if [ -n "$path_directory" ]; then
         echo "Directory $dir found in file: $path_directory" >> "$result_file"
     fi
 done
 
-# Записуємо час завершення виконання скрипта в файл результату
-#echo "Script finished at $(date)" >> "$result_file"
-
-
-# Записуємо час завершення скрипта в файл script.log
+# Записываем время завершения выполнения скрипта в файл результатов
 echo "Script finished at $(date)" >> "$script_log"
-
-#В .bashrc добавив команду bash /home/dnscxrbl/work/script/SOMA щоб скріпт автоматично запускався під час запуску терміналу
-
-#В crontab -e добавив 5 * * * * /home/dnscxrbl/work/script/SOMA/script1.sh щоб скріпт спрацьовув кожну 5 хвилину
-
-#Знаходимо файли у папці 2024 з розширенням .txt, далі вираховуємо які з них старші 3х днів, далі запускаємо цикл,який буде видаляти розширення .txt і архівувати ці файли з їх назвою  
-
-#find /home/dnscxrbl/2024 -type f -name "*.txt" -mtime +3 -exec sh -c 'for file; do tar -czvf "${file%.txt}.tar.gz" "$file"; done' _ {} +
-
-
